a7ae31f5235c95b4e0740c8a753c5a420785b450
Andreas Vögele <andreas@andreasvoegele.com>
2022-03-07 16:33:52 +0100

    Bump version

--- lib/Alien/libmaxminddb.pm
+++ lib/Alien/libmaxminddb.pm
@@ -6,7 +6,7 @@ use 5.016;
 use warnings;
 use utf8;
 
-our $VERSION = 1.003;
+our $VERSION = 1.004;
 
 use parent qw(Alien::Base);
 
@@ -21,7 +21,7 @@ Alien::libmaxminddb - Find or download and install libmaxminddb
 
 =head1 VERSION
 
-version 1.003
+version 1.004
 
 =head1 SYNOPSIS
 
